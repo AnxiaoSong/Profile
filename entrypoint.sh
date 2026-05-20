b8a5b91510fede81327567a8a381596cba654b56
NSS-01 <873748508@qq.com>
2026-05-20 15:14:45 +0800

    chore: clean deploy branch files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd /site
-
-# Host Gemfile.lock is for Windows; regenerate for Linux
-rm -f Gemfile.lock
-bundle install --quiet
-
-exec bundle exec jekyll serve --host 0.0.0.0 --livereload --force_polling
